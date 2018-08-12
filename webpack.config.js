a7c8cc4521fe738ee6d37cae195bb51433fdf737
pangshifeng <l>
2018-08-12 23:57:55 +0800

    打

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,7 @@ module.exports = {
             '/apihost': { // api表示当前项目请求的key
                 // target: 'http://xxxxxxxx:8000', // 代理服务器路径
                 target: 'http://www.ooo0o.com', // 代理服务器路径
+                // pathRewrite: { '^/apihost': '/' }, // 重写路径
                 pathRewrite: { '^/apihost': '/' }, // 重写路径
                 changeOrigin: true
             }
